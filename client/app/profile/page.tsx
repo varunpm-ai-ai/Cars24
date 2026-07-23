43d8a89b3b969ac4ecc117a1aa2343291737826c
varunpm-ai-ai <varunpm132109@gmail.com>
2026-07-23 15:16:02 +0530

    Updated front end

--- client/app/profile/page.tsx
+++ client/app/profile/page.tsx
@@ -1,5 +1,7 @@
+'use client'
+
 import { Bell, Calendar, Car, LogOut, Mail, Settings, User } from "lucide-react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 
 const index = () => {
